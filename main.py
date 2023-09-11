2629e87518fc36bd0d0139fb1f5fde8e99618e9a
sxyWxarder <drxlless@gmail.com>
2023-09-11 19:16:19 +0300

    file to startup

---
+++ main.py
@@ -0,0 +1,22 @@
+# Move each class to separate file + 
+# Read more about classes
+# Read about specs in python
+# write simple spec to check that new Life game initilize field with expected number of cells
+
+## Запуск файла
+
+from src import field  # field# зробити імпорт з одного файла
+
+# Ініціалізація середовища 
+'''
+initField = settings.initField(10,10)
+initField.initField()
+'''
+field = field.Field()
+field.gameField()
+
+
+# 
+
+
+    
